13f4f00271be9fd76596df022285e2fbaf43478c
Jason-Tellez <jason.michael.tellez@gmail.com>
2021-08-13 10:43:42 -0500

    Finished functions

--- acquire.py
+++ acquire.py
@@ -1,42 +1,95 @@
+import pandas as pd
+import numpy as np
 import os
-import env
+from env import host, user, password
 
+###################### Acquire Titanic Data ######################
 
-def get_connection(db, user=env.user, host=env.host, password=env.password):
+def get_connection(db, user=user, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    It takes in a string name of a database as an argument.
+    '''
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+    
+    
+    
+def new_titanic_data():
+    '''
+    This function reads the titanic data from the Codeup db into a df,
+    write it to a csv file, and returns the df.
+    '''
+    # Create SQL query.
+    sql_query = 'SELECT * FROM passengers'
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('titanic_db'))
+    
+    return df
+
+
 
 def get_titanic_data():
-    filename = "titanic.csv"
-    if os.path.isfile(filename):
-        df_titanic = pd.read_csv(filename)
-        return df_titanic
+    '''
+    This function reads in titanic data from Codeup database, writes data to
+    a csv file if a local file does not exist, and returns a df.
+    '''
+    if os.path.isfile('titanic_df.csv'):
+        
+        # If csv file exists, read in data from csv file.
+        df = pd.read_csv('titanic_df.csv', index_col=0)
+        
     else:
-        # read the SQL query into a dataframe
-        df_titanic = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
-        # Write that dataframe to disk for later. Called "caching" the data for later.
-        df_titanic.to_csv(filename)
-        # Return the dataframe to the calling code
-        return df_titanic
+        
+        # Read fresh data from db into a DataFrame.
+        df = new_titanic_data()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('titanic_df.csv')
+        
+    return df
 
-
+###################### Acquire Iris Data ######################
 
 def new_iris_data():
-    filename = "iris_df.csv"
-    if os.path.isfile(filename):
-        df_iris = pd.read_csv(filename)
-        return df_iris
+    '''
+    This function reads the iris data from the Codeup db into a df.
+    '''
     sql_query = """
-                SELECT measurements.measurement_id,
-	                species.species_name,
-                    species.species_id,
-                    measurements.sepal_length,
-                    measurements.sepal_width,
-                    measurements.petal_length,
-                    measurements.petal_width
-                FROM measurements 
-                JOIN species 
-                ON measurements.species_id = species.species_id;
+                SELECT 
+                    species_id,
+                    species_name,
+                    sepal_length,
+                    sepal_width,
+                    petal_length,
+                    petal_width
+                FROM measurements
+                JOIN species USING(species_id)
                 """
-    df_iris = pd.read_sql(sql_query, get_connection('iris_db'))
-    df_iris.to_csv(filename)
-    return df_iris
+    
+    # Read in DataFrame from Codeup db.
+    df = pd.read_sql(sql_query, get_connection('iris_db'))
+    
+    return df
+
+
+def get_iris_data():
+    '''
+    This function reads in iris data from Codeup database, writes data to
+    a csv file if a local file does not exist, and returns a df.
+    '''
+    if os.path.isfile('iris_df.csv'):
+        
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('iris_df.csv', index_col=0)
+        
+    else:
+        
+        # Read fresh data from db into a DataFrame
+        df = new_iris_data()
+        
+        # Cache data
+        df.to_csv('iris_df.csv')
+        
+    return df
