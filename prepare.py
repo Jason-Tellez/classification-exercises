3f242e312206f954b6dbb5edede0b0afa2032f31
Jason-Tellez <jason.michael.tellez@gmail.com>
2021-08-17 16:09:09 -0500

    Added clean and train functions

--- prepare.py
+++ prepare.py
@@ -1,3 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import splitting and imputing functions
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+# turn off pink boxes for demo
+import warnings
+warnings.filterwarnings("ignore")
+
+# import our own acquire module
+import acquire
+
+
 def prep_iris(df):
     """
     Takes in data from the iris db and turns it into a dataframe.
@@ -10,3 +26,28 @@ def prep_iris(df):
     df_dummy = pd.get_dummies(df['species'])
     df = pd.concat([df, df_dummy], axis=1)
     return df
+
+def clean_data(df):
+    '''
+    This function will drop any duplicate observations, 
+    drop ['deck', 'embarked', 'class', 'age'], fill missing embark_town with 'Southampton'
+    and create dummy vars from sex and embark_town. 
+    '''
+    df = df.drop_duplicates()
+    df = df.drop(columns=['deck', 'embarked', 'class', 'age'])
+    df['embark_town'] = df.embark_town.fillna(value='Southampton')
+    dummy_df = pd.get_dummies(df[['sex', 'embark_town']], drop_first=True)
+    df = pd.concat([df, dummy_df], axis=1)
+    return df
+
+def split_data(df):
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames; stratify on survived.
+    return train, validate, test DataFrames.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df.survived)
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123, 
+                                       stratify=train_validate.survived)
+    return train, validate, test
